4f4623224fc61686661bd4fb794aaf7dcbff8d1d
zakuro <z@kuro.red>
2026-08-02 09:17:26 +0900

    add Home view

---
+++ src/views/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const v = ref('text')
+</script>
+<template>
+  <h1>home</h1>
+  <div>
+    <input v-model="v" />
+    <p>{{ v }}</p>
+  </div>
+</template>
